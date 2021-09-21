5e4b88f9cf1ba172f58fc0e80678b9ae39bafb76
ermaleyalekov <ermaleyalekov@yahoo.com>
2021-09-21 12:27:40 +0300

    getenv

--- sources/env_utils.c
+++ sources/env_utils.c
@@ -7,7 +7,7 @@ char    *ft_getenv(char *name)
     ale = g_sh.listEnv;
     while (ale)
     {
-        if (!strncmp(name, ale->name, ft_strlen(ale->name) + 1))
+        if (!ft_strncmp(name, ale->name, ft_strlen(ale->name) + 1))
             return (ale->value);
         ale = ale->next;
     }
